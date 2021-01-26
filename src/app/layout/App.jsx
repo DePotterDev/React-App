5e71b5106eb55660e1a3b6081d9adf51ab10d736
manalone <depotter.laurens@gmail.com>
2021-01-25 23:13:59 -0300

    Added structure

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,14 @@
 
+// anonymous functions
+// export default function(){}
 
 function App() {
   return (
     <div className="App">
       <h1>Sul Festa</h1>
+      <button className='ui icon red button'>
+        <i className='user icon'/> CSS Button
+      </button>
     </div>
   );
 }
