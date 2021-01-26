25ee7a9990f50b973607fe54815b497fc6cfd67d
manalone <depotter.laurens@gmail.com>
2021-01-25 23:33:56 -0300

    Added EventDashboard

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,13 +2,14 @@
 // anonymous functions
 // export default function(){}
 
+
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+
 function App() {
   return (
     <div className="App">
       <h1>Sul Festa</h1>
-      <button className='ui icon red button'>
-        <i className='user icon'/> CSS Button
-      </button>
+      <EventDashboard/>
     </div>
   );
 }
